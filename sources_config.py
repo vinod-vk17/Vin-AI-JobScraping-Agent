b96d0a26b2fb6f5ce9f77d70451924de55f294a9
Vinod Prakash <vinodkumar.prakash17@gmail.com>
2026-02-11 16:08:00 -0500

    Update sources_config.py

--- sources_config.py
+++ sources_config.py
@@ -22,57 +22,7 @@ SOURCES = {
                 "https://careers.google.com/jobs/results/",
             ]
         }
-    },
-    
-    "ENERGY": {
-        "companies": {
-            "Shell": [
-                "https://www.shell.com/careers/about-careers-at-shell/current-opportunities.html",
-            ],
-            "BP": [
-                "https://www.bp.com/en/global/corporate/careers/search-jobs.html",
-            ],
-            "ExxonMobil": [
-                "https://corporate.exxonmobil.com/careers/find-a-job",
-            ],
-            "Chevron": [
-                "https://careers.chevron.com/search",
-            ],
-            "TotalEnergies": [
-                "https://www.totalenergies.com/careers/join-us",
-            ],
-        }
-    },
-    
-    "BFSI": {
-        "companies": {
-            "JPMorgan Chase": [
-                "https://careers.jpmorgan.com/us/en/students/programs",
-                "https://jpmc.fa.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001",
-            ],
-            "Goldman Sachs": [
-                "https://www.goldmansachs.com/careers/",
-            ],
-            "Bank of America": [
-                "https://careers.bankofamerica.com/en-us/search-jobs",
-            ],
-            "Morgan Stanley": [
-                "https://www.morganstanley.com/careers/",
-            ],
-            "Citigroup": [
-                "https://jobs.citi.com/",
-            ],
-        }
-    },
-    
-    # Add your own categories here!
-    # "CONSULTING": {
-    #     "companies": {
-    #         "McKinsey": ["https://www.mckinsey.com/careers"],
-    #         "BCG": ["https://careers.bcg.com/"],
-    #         "Bain": ["https://www.bain.com/careers/"],
-    #     }
-    # },
+    }
 }
 
 
